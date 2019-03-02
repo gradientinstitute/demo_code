55c0ebff6225e664f6cecfd8e5981ab73a8917c4
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2019-03-02 15:37:16 +1100

    WIP

--- blog_post_4/main.py
+++ blog_post_4/main.py
@@ -1,20 +1,38 @@
+# Import libraries:
+
 
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as pl
 
-n = 10000 # number of unit records
-n_select = 100
-frac_green = 0.5
 
-def generate_data(n):
-    a = 0.5
-    b = 10
-    # X1 = np.random.gamma(a, b, size=n)
-    X1 = np.random.uniform(low=0., high=50., size=n)
-    X2 = np.random.choice([0,1], size=n, p=[1 - frac_green, frac_green])
-    return np.vstack((X1, X2)).T
+n = 10000 # number of unit records
+frac_green = 0.5  # fraction of population that are green
+def generate_data(n, delta_loc_green=0.1, delta_scale_green=0,
+                  seed=1, frac_green=0.5):
+    loc = 0.0
+    scale = 1.0
+    n_green = int(frac_green * n)
+    n_nogreen = n - n_green
+    rnd = np.random.RandomState(seed=1)
+    X_isgreen = np.zeros(n, dtype=bool)
+    X_isgreen[0:n_green] = True
+    rnd.shuffle(X_isgreen)
+    X1_green = rnd.normal(loc=(loc + delta_loc_green),
+                          scale=(scale + delta_scale_green), size=n_green)
+    X1_nogreen = rnd.normal(loc=loc, scale=scale, size=n_nogreen)
+    X1 = np.zeros(n)
+    X1[X_isgreen] = X1_green
+    X1[~X_isgreen] = X1_nogreen
+    return np.vstack((X1, X_isgreen)).T
+
+X_latent = generate_data(n, seed=1)
+isgreen = X_latent[:, 1].astype(bool)
+# pl.figure(figsize=(5,2.5), dpi=150)
+# pl.hist(X_latent[:, 0][isgreen], bins=20)
+# pl.hist(X_latent[:, 0][~isgreen], bins=20)
+
 
 def logistic(x, L, k, x_0):
     r = L / (1 + np.exp( -1 * k * (x - x_0) ))
@@ -22,54 +40,107 @@ def logistic(x, L, k, x_0):
 
 def true_prob(X):
     L = 0.75
-    k = 1
-    x_0 = 10.0
-    base_rate = 0.8
-    X_noise = X[:, 0] + np.random.normal(scale=1.0, size=X.shape[0])
-    p1 = base_rate - logistic(X_noise, L, k, x_0)
-    results = np.random.uniform(size=p1.shape) < p1
-    return p1, results
+    k = 2
+    x0 = 1.0
+    base_rate = 0.05
+    p = base_rate + logistic(X[:,0], L, k, x0)
+    return p
+
+P = true_prob(X_latent)
 
 
-X = generate_data(n)
-p, Y = true_prob(X)
+def sample_results(p, seed=0):
+    rnd = np.random.RandomState(seed)
+    results = rnd.uniform(size=p.shape) < p
+    return results
 
-model = LogisticRegression(solver='lbfgs')
-# model = LogisticRegression(class_weight='balanced', solver='lbfgs')
-# model = SVC(probability=True, kernel='poly')
-model.fit(X, Y)
-p_est = model.predict_proba(X)[:, 1]
-
-
-# rank and select the top
-rank = np.argsort(p_est)[::-1]
-p_rank = p_est[rank][0:n_select]
-p_true = p[rank][0:n_select]
-y_true = Y[rank][0:n_select]
-acc = np.sum(y_true) / y_true.shape[0]
-
-# small change re. gender skews rankings
-# draws of noise totally change rangkings
-# discontinuity implies invidivual fairness not satisfied
-# accuracy/cost slightly better with rankings
-# covariate shift on the relative propensity of the genders
-# ranking means you never observe most of the space
-# plot error rate vs size of samples -- curve has different behaviour
-# conceptually, difference between auditing and university admission -- could
-# terry tao miss out at uni?
-# is there some explict work here on the probability our model is wrong
-# doing utility function question on the utils
-# define costs for false positives and false negatives then see what happens
-# over the whole cohort
+# Note I'm getting the true prob here
+P = true_prob(X_latent)
+Y = sample_results(P)
 
 
+
+
+
+def draw_observations(X_latent, seed=1, loc=0.0, scale=0.3):
+    rnd = np.random.RandomState(seed)
+    X_noise = X_latent[:, 0] + rnd.normal(loc=loc, scale=scale, size=X_latent.shape[0])
+    return np.vstack((X_noise, X_latent[:,1])).T
+
+X = draw_observations(X_latent)
+
+# pl.figure()
+# pl.scatter(X[:, 0], P)
+# pl.scatter(X[:, 0], Y)
+# pl.show()
+
+
+
+
+n_select = 100
+
+def train_test(X, Y, P):
+    X_train = X[:n//2]
+    X_test = X[n//2:]
+    Y_train = Y[:n//2]
+    Y_test = Y[n//2:]
+    P_train = Y[n//2:]
+    P_test = Y[:n//2]
+    return X_train, X_test, Y_train, Y_test, P_train, P_test
+
+def estimate(X_train, Y_train, X_test):
+    model = LogisticRegression(solver='lbfgs')
+    # model = LogisticRegression(class_weight='balanced', solver='lbfgs')
+    # model = SVC(probability=True, kernel='poly')
+    model.fit(X_train, Y_train)
+    p_est = model.predict_proba(X_test)[:, 1]
+    return p_est
+
+
+def rank_eval(p, X_test, Y_test, P_test):
+    rank = np.argsort(p)[::-1]
+    p_rank = p[rank][0:n_select]
+    X_rank = X_test[rank][0:n_select]
+    Y_rank = Y_test[rank][0:n_select]
+    P_true_rank = P_test[rank][0:n_select]
+    acc = np.sum(Y_rank) / Y_rank.shape[0]
+    frac_green = np.sum(X_rank[:, 1]) / n_select
+    return acc, frac_green
+
+def baserate_selection_plot():
+    baserates = np.linspace(0, 1, 20)
+    out = [baserate_rank(b) for b in baserates]
+    acc_vector, bias_vector, tot_vec = zip(*out)
+    return baserates, acc_vector, bias_vector, tot_vec
+
+def baserate_rank(b):
+    Xl = generate_data(n, delta_loc_green=b)
+    P = true_prob(Xl)
+    Y = sample_results(P)
+
+    print("bias: {}".format(b))
+    print("total positive rate: {}".format(np.sum(Y) / Y.shape[0]))
+    i_green = Xl[:,1] ==1.0
+    Y_green = Y[i_green]
+    Y_nogreen = Y[~i_green]
+    n_green = Y_green.shape[0]
+    n_nogreen = Y_nogreen.shape[0]
+    print("green positive rate: {}".format(np.sum(Y_green) / n_green))
+    print("nongreen positive rate: {}".format(np.sum(Y_nogreen) / n_nogreen))
+
+    X = draw_observations(Xl)
+    X, Xs, Y, Ys, P, Ps = train_test(X, Y, P)
+    Pe = estimate(X, Y, Xs)
+    acc, frac_green = rank_eval(Pe, Xs, Ys, Ps)
+
+    n_green = np.sum(Xs[:, 1])
+    n_green_guilty = np.sum(Ys[(Xs[:, 1]).astype(bool)])
+    total_frac_green = n_green_guilty / n_green
+    return acc, frac_green, total_frac_green
+
+
+baserates, acc_vector, bias_vector, tot_vec = baserate_selection_plot()
 pl.figure()
-pl.scatter(X[:, 0], p)
-pl.scatter(X[:, 0], Y)
-pl.scatter(X[:, 0], p_est)
+pl.plot(baserates, bias_vector, 'r-')
+pl.plot(baserates, tot_vec, 'k-')
 pl.show()
-
-pl.figure()
-pl.scatter(p, p_est)
-pl.show()
-
